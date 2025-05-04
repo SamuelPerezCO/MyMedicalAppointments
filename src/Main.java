9fe3b491befd22e4fecc3bcce71bb6e14e41cc2e
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-05-03 19:32:02 -0500

    Clase 8

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor("Anahí Salgado");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Alejandro Rodríguez";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+
+        showMenu();
 
     }
+
 }
