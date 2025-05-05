6e898de68da98045e6aeba7bb1f330996a9f7395
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-05-05 18:42:13 -0500

    Clase 12

---
+++ src/Patient.java
@@ -0,0 +1,17 @@
+public class Patient {
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String birthday;
+    private String blood;
+    private double weight;
+    private double height;
+
+    Patient(String name, String email){
+        this.name = name;
+        this.email = email;
+        this.weight = 54.5;
+        System.out.println(weight + " KG");
+    }
+}
