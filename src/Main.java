9fda7ff49f7c39e51a34e345bc6ced9f4532ea34
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-05-04 20:03:54 -0500

    Clase 10

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,11 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Alejandro Rodríguez";
-        myDoctor.showName();
-        myDoctor.showId();
+        //showMenu();
 
-        Doctor myDoctorAnn = new Doctor();
-        myDoctor.showId();
-
-        showMenu();
+        Doctor myDoctor = new Doctor("Anahi Salgado" , "Pediatria");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
 
     }
 
