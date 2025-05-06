78c642aebcba4c522e47f4eff1f43a169768385c
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-05-06 17:42:06 -0500

    Clase 16

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,30 @@ public class Main {
         System.out.println(myDoctor.name);
         System.out.println(myDoctor.speciality);
 
-        Patient patient = new Patient("Alejandra" , "alejandra@email.com");
-        patient.setWeight(54.6);
-        System.out.println(patient.getWeight());
+        int i = 0;
+        int b = 2;
+        b = i;
+        //b = 0
+        String name = "Ann";
 
-        patient.setPhoneNumber("12345678");
-        System.out.println(patient.getPhoneNumber());
+        Patient patient = new Patient("Alejandra" , "alejandra@email.com");
+        Patient patient2 = new Patient("Anahi" , "anahi@email.com");
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        patient2.setName("Manuel");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+//        patient.setWeight(54.6);
+//        System.out.println(patient.getWeight());
+//
+//        patient.setPhoneNumber("12345678");
+//        System.out.println(patient.getPhoneNumber());
     }
 
 }
